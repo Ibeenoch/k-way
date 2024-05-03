3ee5df18a0ad64c8d6862173f2c6e1ea452675c0
Ibewunjo Enoch <fredenoch1@gmail.com>
2024-05-03 04:38:43 -0700

    make api active

--- src/Loading.tsx
+++ src/Loading.tsx
@@ -19,7 +19,7 @@ const Loading: React.FC = () => {
           className: 'text-red-800'
         })
       }
-      
+
     }, 1000 );
 
     return () => clearInterval(timer);
@@ -28,7 +28,8 @@ const Loading: React.FC = () => {
 
   return (
     <div style={{ display: 'flex', color: 'black', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', opacity: 0.3}}>
-<p>Please be patience for {count} seconds, this may take a while...</p>
+<p>Please be patience for {count} seconds</p>
+<p>this may take a while...</p>
 <svg xmlns="http://www.w3.org/2000/svg" style={{ width: '150px', height: '150px' }} viewBox="0 0 200 200"><linearGradient id="a12"><stop offset="0" stop-color="#E50202" stop-opacity="0"></stop><stop offset="1" stop-color="#E50202"></stop></linearGradient><circle fill="none" stroke="url(#a12)" stroke-width="11" stroke-linecap="round" stroke-dasharray="0 44 0 44 0 44 0 44 0 360" cx="100" cy="100" r="70" transform-origin="center"><animateTransform type="rotate" attributeName="transform" calcMode="discrete" dur="2" values="360;324;288;252;216;180;144;108;72;36" repeatCount="indefinite"></animateTransform></circle></svg>    
 </div>
   )
