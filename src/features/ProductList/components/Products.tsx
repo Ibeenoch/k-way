c79e1a9e7e5b81a552f6fee84e585b5f26c01d58
Ibewunjo Enoch <fredenoch1@gmail.com>
2024-05-03 05:02:14 -0700

    make api active

--- src/features/ProductList/components/Products.tsx
+++ src/features/ProductList/components/Products.tsx
@@ -576,7 +576,7 @@ const Products: React.FC<ItogglePopup> = ({ isOpen, togglePopup }) => {
                     </button>
                     <button
                       type="button"
-                      className="ml-4 p-2 text-white hover:text-gray-300 sm:ml-6 lg:hidden"
+                      className="ml-4 p-2 hover:text-gray-300 sm:ml-6 lg:hidden"
                       onClick={() => setMobileFiltersOpen(true)}
                     >
                       <span className="sr-only">Filters By</span>
@@ -681,7 +681,7 @@ const Products: React.FC<ItogglePopup> = ({ isOpen, togglePopup }) => {
                           </h2>
 
                           {/* <div className="mt-1 grid grid-cols-1 gap-x-1 gap-y-3 sm:grid-cols-3 px-1  py-2 lg:grid-cols-3 xl:gap-x-4"> */}
-                            <div className="mt-1 grid grid-cols-3 gap-x-2 gap-y-2 sm:grid-cols-3 lg:grid-cols-4 xl:gap-x-8">
+                            <div className="mt-1 grid grid-cols-2 gap-x-2 gap-y-2 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-4">
                             {products && Array.isArray(products) ? (
                               products.map((product: any, index: number) => (
                                 <div
