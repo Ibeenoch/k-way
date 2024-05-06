69b94a95d005ba5c2d48be5cc1c54a7baa36e1ab
Ibewunjo Enoch <fredenoch1@gmail.com>
2024-05-06 13:52:34 -0700

    added documentation, and landing page

--- src/features/doc/Starter.tsx
+++ src/features/doc/Starter.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import './Starter.css'
 import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from '../../app/hooks'
@@ -16,7 +16,11 @@ const handleNextPage = () => {
     })
 }
 
-const handleNewsLetter = () => {
+const handleNewsLetter = (e: FormEvent) => {
+    e.preventDefault();
+    if(!news){
+        return
+    }
    setNews('')
    toast.success('thank you for subscribing to our news letter', {
     position: 'top-center',
@@ -86,10 +90,11 @@ const handleNewsLetter = () => {
         >
             Getting Started
         </button>
+        <br /> <br />
         </p>
 
         
-        <form className="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
+        <form onSubmit={handleNewsLetter} className="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label
               className="block text-blue-300 py-2 font-bold mb-2"
@@ -100,17 +105,17 @@ const handleNewsLetter = () => {
             <input
               className="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
               id="emailaddress"
-              type="text"
+              type="email"
               onChange={(e) => setNews(e.target.value)}
               value={news}
               placeholder="you@somewhere.com"
+              required
             />
           </div>
           <div className="flex items-center justify-between pt-4">
             <button
-            onClick={handleNewsLetter}
               className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-              type="button"
+              type="submit"
             >
               Sign Up
             </button>
